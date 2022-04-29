8c7ed4b08d81beb9d25e63589cb0b886274b7f4c
장동환 <93087571+woumha@users.noreply.github.com>
2022-04-30 00:18:30 +0900

    Add files via upload

--- js/10000hours.js
+++ js/10000hours.js
@@ -8,18 +8,18 @@ const loading = document.querySelector(".result_loading");
 
 function calculator() {
     const fieldValue = document.querySelector("#field_value");
-    let timeValue = document.querySelector("#time_value");
-    let timeValue_int = Number(timeValue.value);
+    const timeValue = document.querySelector("#time_value"); /*"#time_value"요소는 변하지않기 때문에 let에서 const로 변경 */
+    const timeValue_int = Number(timeValue.value); /*값이 적용된 뒤로 바뀔 일이 없기 때문에 역시 let에서 const로 변경 */
 
     const fieldResult = document.querySelector(".field_result");
     const timeResult = document.querySelector(".time_result");
 
     if(fieldValue.value == ""){
-        alert('입력되지 않았습니다.');
+        alert('분야가 입력되지 않았습니다.'); /* 두개의 오류창이 입력되지 않았습니다 라고만 되어있어 정보가 확실하지않음, 확실하게 정보전달!*/
         fieldValue.focus();
         return false;
     } else if(timeValue.value == ""){
-        alert('입력되지 않았습니다.');
+        alert('시간이 입력되지 않았습니다.');
         timeValue.focus();
         return false;
     } else if(timeValue_int > 24) {
@@ -31,10 +31,13 @@ function calculator() {
     loading.style.display = "flex";
 
     setTimeout(function() {
-        loading.style.display = "none";
-        result.style.display = "flex";
         fieldResult.innerText = fieldValue.value;
         timeResult.innerText = parseInt((10000/timeValue_int), 10);
+        loading.style.display = "none";
+        result.style.display = "flex";
+/*      fieldResult.innerText = fieldValue.value;
+        timeResult.innerText = parseInt((10000/timeValue_int), 10); 
+        위로 이동!*/
     }, 1800); 
 }
 /* 1800=1.8초 뒤에 실행된다는 뜻 */
@@ -82,5 +85,5 @@ openButton.addEventListener('click', openModal);
 closeButton.addEventListener('click', closeModal);
 startButton.addEventListener('click', calculator);
 
-/* addEventListener 를 addEventListner 로 적어서 자바스크립트 오류가 계속 났었음 e를 빼먹음 */
+/* addEventListener 를 addEventListner 로 적어서 자바스크립트 오류가 계속 났었음. e를 빼먹음 */
 
